550ba693198038d65ad58a39297bf2762957ce98
Chanida Clarkston <chanida@Chanidas-MacBook-Air.local>
2018-04-04 10:59:43 -0600

    Added grid to help with content layout

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ require('./App.css');
 const pageNumber = 1;
 
 const successDiv = (
+  <Grid>
+  <Row>
+    <Col>
   <ContentCenter>
     <Success>Thank you for registering. Once your donation has been made on my <a href="http://pages.mwoy.org/rm/denver18/cclarkston" target="_blank" rel="noopener noreferrer">official campainge donation page</a>, we will send you a confirmation email!</Success>
   </ContentCenter>    
+  </Col>
+  </Row>
+  </Grid>
 );
 /*
       <Row>
@@ -25,6 +31,9 @@ const successDiv = (
       */
 const homeDiv = (
   <Home>
+            <Grid>
+          <Row>
+            <Col>
     <ContentCenter>
     <Image responsive src="./imgs/BagsForTheCure-Logo.png" align="center" style={{margin: '0 auto'}} />
     <H3>A funraising event benefiting The Leukemia and Lymphoma Society</H3>
@@ -45,11 +54,17 @@ const homeDiv = (
       </Row>
     </Grid>  
     </ContentCenter>
+    </Col>
+      </Row>
+    </Grid>  
   </Home>
 );
 
 const prizesDiv = (
   <Prizes>
+    <Grid>
+      <Row>
+        <Col>
     <ContentCenter>
       <H2>A big THANK YOU for our donors!</H2>
       <H3>What can I win from the raffle?</H3>
@@ -65,11 +80,17 @@ const prizesDiv = (
         <li>Check back as we add to this list!</li>
       </ul>
     </ContentCenter>
+    </Col>
+      </Row>
+    </Grid>  
   </Prizes>
 );
 
 const llsMission = (
-  <ContentCenter>
+  <Grid>
+  <Row>
+    <Col>
+  <Content>
     The mission of The Leukemia & Lymphoma Society (LLS) is: Cure leukemia, lymphoma, Hodgkin's disease and myeloma, and improve the quality of life of patients and their families.
     <br />
     <a href="LLSMissionFacts.pdf" target="_blank">
@@ -77,11 +98,17 @@ const llsMission = (
         <Page pageNumber={pageNumber} />
       </Document> 
     </a>
-  </ContentCenter>
+  </Content>
+  </Col>
+  </Row>
+  </Grid>
 );
 
 const diveInn = (
   <DiveInn>
+        <Grid>
+      <Row>
+        <Col>
     <ContentCenter>
       <H2>The Dive Inn</H2>
       <H3>Denver's #1 Neighborhood Bar. Come and see for yourself.</H3>
@@ -100,6 +127,9 @@ const diveInn = (
       </Row>
     </Grid>       
     </ContentCenter>
+    </Col>
+      </Row>
+        </Grid>
   </DiveInn>
 )
 
@@ -163,39 +193,48 @@ class App extends React.Component {
 
     let register = (
       <Register>
-        <Content>
-          <H2>Register Your Team!</H2>  
-          To register your 2-person team, please submit the form below and make a <b>Tax-Deductible</b> $50 donation to my <a href="http://pages.mwoy.org/rm/denver18/cclarkston" target="_blank" rel="noopener noreferrer">official campainge donation page</a>. An email will be sent once your registration has been confirmed.<br />
-          All donations go directly to The Leukemia & Lymphoma Society<br />
-          Thank you!!!<br /><br />
-          <div style={this.state.formVisibility}>
-          <form className="contact-form" onSubmit={this.handleSubmit}>
-              <div className="form-field">
-                <label htmlFor="name">
-                  <div className="label-content">Team Name:</div>
-                  <input type="text" name="name" value={this.state.name} onChange={this.handleNameChange} required />
-                </label>
-              </div>
-              <div className="form-field">
-                <label htmlFor="email">
-                  <div className="label-content">Email:</div>
-                  <input type="email" name="email" value={this.state.email} onChange={this.handleEmailChange} required />
-                </label>
-              </div>
-              <div className="form-field">
-                <label htmlFor="phonenumber">
-                  <div className="label-content">Phone number:</div>
-                  <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handlePhoneNumberChange} required />
-                </label>
-              </div>
-              <button type="submit">Send</button>
-            </form>
-          </div>  
-        </Content>
+        <Grid>
+          <Row>
+            <Col>
+            <Content>
+              <H2>Register Your Team!</H2>  
+              To register your 2-person team, please submit the form below and make a <b>Tax-Deductible</b> $50 donation to my <a href="http://pages.mwoy.org/rm/denver18/cclarkston" target="_blank" rel="noopener noreferrer">official campainge donation page</a>. An email will be sent once your registration has been confirmed.<br />
+              All donations go directly to The Leukemia & Lymphoma Society<br />
+              Thank you!!!<br /><br />
+              <div style={this.state.formVisibility}>
+              <form className="contact-form" onSubmit={this.handleSubmit}>
+                  <div className="form-field">
+                    <label htmlFor="name">
+                      <div className="label-content">Team Name:</div>
+                      <input type="text" name="name" value={this.state.name} onChange={this.handleNameChange} required />
+                    </label>
+                  </div>
+                  <div className="form-field">
+                    <label htmlFor="email">
+                      <div className="label-content">Email:</div>
+                      <input type="email" name="email" value={this.state.email} onChange={this.handleEmailChange} required />
+                    </label>
+                  </div>
+                  <div className="form-field">
+                    <label htmlFor="phonenumber">
+                      <div className="label-content">Phone number:</div>
+                      <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handlePhoneNumberChange} required />
+                    </label>
+                  </div>
+                  <button type="submit">Send</button>
+                </form>
+              </div>  
+            </Content>
+            </Col>
+          </Row>
+        </Grid>            
       </Register>
     );
 
     const detailsDiv = (
+      <Grid>
+        <Row>
+          <Col>
       <Content>
         <Image responsive src="./imgs/LLSMWLogo.jpg" style={floatRight} />
         Come have some fun in the sun and join us for Bags for the Cure, a Corn-Hole Tournament benefiting The Leukemia & Lymphoma Society (LLS). Tournament participants will receive their first draft on the house and entries into our raffle for prizes. 
@@ -216,6 +255,9 @@ class App extends React.Component {
         <br />
         Any questions, please send a message to bagsforthecure@gmail.com
       </Content>
+      </Col>
+      </Row>
+      </Grid>
     );
        
     const responsiveNavBar = (
